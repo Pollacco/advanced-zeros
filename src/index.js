5153ade2b40568f60a2f0f590601384263f7e791
Andrey <andrey.sviontek@gmail.com>
2019-03-15 09:25:33 +0500

    feat: any% completed in advanced zeros

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 module.exports = function getZerosCount(number, base) {
   // your implementation
+  let power = Math.floor(Math.log(number)/Math.log(base));
+  let sum=0;
+  for(let i=1; i<=power; i++){
+    let num=Math.pow(base,i);
+    sum+=Math.floor(number/num);
+  }
+  return sum;
 }
