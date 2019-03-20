5e3ebdd41e6b8b8e259ee83b4e3888c550bdc5d8
Andrey <andrey.sviontek@gmail.com>
2019-03-20 10:03:01 +0500

    fix: almost complete task

--- src/index.js
+++ src/index.js
@@ -1,10 +1,20 @@
 module.exports = function getZerosCount(number, base) {
-  // your implementation
-  let power = Math.floor(Math.log(number)/Math.log(base));
-  let sum=0;
-  for(let i=1; i<=power; i++){
-    let num=Math.pow(base,i);
-    sum+=Math.floor(number/num);
+
+  for (let i = 2; i <= base; i++) {
+    if (base % i === 0) {
+      let exponentIndex = 0;
+      while (base % i === 0) {
+        base = base / i;     
+        exponentIndex++;
+      }
+      let multiplier = 0;
+      let primesInNumber = Math.floor(number / i);
+      while (primesInNumber > 0) {
+        multiplier += primesInNumber;
+        primesInNumber = Math.floor(primesInNumber / i);
+      }
+      zeros = Math.min(Math.floor(multiplier / exponentIndex))
+    }
   }
-  return sum;
+  return zeros;
 }
